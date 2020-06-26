5df1fa9bd5b33740444d8cbd00aa317c39046ce4
Dat <dat.nguyen@journeyh.io>
2020-06-26 14:00:35 +0700

    add class SideBar

--- src/features/sideBar/SideBar.js
+++ src/features/sideBar/SideBar.js
@@ -1,67 +1,94 @@
 import React, { useState, useEffect } from "react";
-import classNames from 'classnames';
-import styles from './SideBar.module.css';
+import classNames from "classnames";
+import styles from "./SideBar.module.css";
 
-const SideBarItem = ({
-  info,
-  isClicked,
-  onClick
-}) => {
-
+const SideBarItem = ({ info, isClicked, onClick }) => {
   return (
-    <div className={classNames(
-      styles["side-bar-item"], {
+    <div
+      className={classNames(styles["side-bar-item"], {
         [styles.clicked]: isClicked === info.name,
-      }
-    )}
-    onClick={() => onClick(info.name)}
+      })}
+      onClick={() => onClick(info.name)}
     >
       {info.name}
     </div>
   );
-}
+};
 
-const ItemSideBar = ({
-  itemLists,
-  isClicked,
-  onClick
-}) => {
-  
-  return itemLists 
-  ? itemLists.map(item => (
-    <SideBarItem key={item.name ? item.name : ''} info={item} isClicked={isClicked} onClick={onClick}/>
-  ))
-  : null;
-}
+const ItemSideBar = ({ itemLists, isClicked, onClick }) => {
+  return itemLists
+    ? itemLists.map((item) => (
+        <SideBarItem
+          key={item.name ? item.name : ""}
+          info={item}
+          isClicked={isClicked}
+          onClick={onClick}
+        />
+      ))
+    : null;
+};
 
-function SideBar({
-  itemSideBarInfoList
-}) {
-  
-  const [isClicked, setIsClicked] = useState("Vietnam");
+// function SideBar({
+//   itemSideBarInfoList
+// }) {
 
-  function clickItem(name) {
-    setIsClicked(name);
+//   const [isClicked, setIsClicked] = useState("Vietnam");
+
+//   function clickItem(name) {
+//     setIsClicked(name);
+//     console.log("name click: ", name);
+//   }
+
+//   useEffect(() => {
+
+//   })
+
+//   return (
+//     <div className={styles["wrapper-side-bar"]}>
+//       <div className={styles["logo-wrapper"]}>
+//         <div className={styles["logo"]}></div>
+//         <p>Covid-19 app</p>
+//       </div>
+//       <ItemSideBar
+//         itemLists={itemSideBarInfoList}
+//         isClicked={isClicked}
+//         onClick={clickItem}
+//       />
+//     </div>
+//   );
+// }
+
+class SideBar extends React.Component {
+  constructor({ itemSideBarInfoList }) {
+    super({
+      itemSideBarInfoList,
+    });
+    this.clickItem = this.clickItem.bind(this);
+    this.state = {
+      isClicked: "Vietnam",
+    };
+  }
+
+  clickItem(name) {
+    this.setIsClicked(name);
     console.log("name click: ", name);
   }
 
-  useEffect(() => {
-
-  })
-
-  return (
-    <div className={styles["wrapper-side-bar"]}>
-      <div className={styles["logo-wrapper"]}>
-        <div className={styles["logo"]}></div>
-        <p>Covid-19 app</p>
+  render() {
+    return (
+      <div className={styles["wrapper-side-bar"]}>
+        <div className={styles["logo-wrapper"]}>
+          <div className={styles["logo"]}></div>
+          <p>Covid-19 app</p>
+        </div>
+        <ItemSideBar
+          itemLists={this.itemSideBarInfoList}
+          isClicked={this.isClicked}
+          onClick={this.clickItem}
+        />
       </div>
-      <ItemSideBar
-        itemLists={itemSideBarInfoList}
-        isClicked={isClicked}
-        onClick={clickItem}
-      />
-    </div>
-  );
+    );
+  }
 }
 
 export default SideBar;
